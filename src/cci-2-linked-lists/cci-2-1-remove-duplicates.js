2e878c6c182e6914428ab2901e94a8fba8c895ef
Peter D Lee <dhlee9111@gmail.com>
2021-03-27 22:01:01 +0900

    UPDATE: cci-2-1 method 2 added (without temp buffer)

--- src/cci-2-linked-lists/cci-2-1-remove-duplicates.js
+++ src/cci-2-linked-lists/cci-2-1-remove-duplicates.js
@@ -7,6 +7,7 @@
 
 import { SLinkedList } from "../ds/linkedlist-singly.js";
 
+// 1. Using hash table
 function removeDup(sll) {
   let seenVal = new Map();
   seenVal.set(sll.head.value, true);
@@ -16,7 +17,8 @@ function removeDup(sll) {
     if (!seenVal.has(currentNode.next.value)) {
       seenVal.set(currentNode.next.value, true);
       currentNode = currentNode.next;
-    } else {
+    } 
+    else {
       currentNode.next = currentNode.next.next;
       if (currentNode.next === null) sll.tail = currentNode;
     }
@@ -59,3 +61,65 @@ ll2.append(6);
 ll2.append(3);
 ll2.append(6);
 console.log("input: ", ll2.showArray(), "solution: ", removeDup(ll2).showArray()); // [0, 5, 3, 2, 1, 4, 6]
+
+
+//---------------------------------------------------------------------
+
+// 2. Without extra space
+function removeDupBF(sll) {
+  let currentPointer = sll.head;
+  let runnerPointer = currentPointer;
+
+  while (currentPointer.next !== null) {
+    while (runnerPointer.next !== null) {
+      if (currentPointer.value !== runnerPointer.next.value) {
+        runnerPointer = runnerPointer.next === null ? runnerPointer : runnerPointer.next;
+      }
+      else {
+        runnerPointer.next = runnerPointer.next.next;
+      }
+      if (runnerPointer.next === null) {
+        currentPointer = currentPointer.next;
+        runnerPointer = currentPointer;
+        if (runnerPointer === null) break;
+      }
+    }
+    if (currentPointer === null) break;
+  }
+
+  return sll;
+}
+// T: O(n^2)
+// S: O(1)
+
+
+// TEST
+// [1, 0, 3, 2, 1, 5, 1, 2, 6, 6]
+const ll3 = new SLinkedList(1);
+ll3.append(0);
+ll3.append(3);
+ll3.append(2);
+ll3.append(1);
+ll3.append(5);
+ll3.append(1);
+ll3.append(2);
+ll3.append(6);
+ll3.append(6);
+console.log("input: ", ll3.showArray(), "solution: ", removeDupBF(ll3).showArray()); // [1, 0, 3, 2, 5, 6]
+
+// [0, 0, 0, 5, 3, 2, 1, 4, 3, 2, 2, 6, 3, 6]
+const ll4 = new SLinkedList(0);
+ll4.append(0);
+ll4.append(0);
+ll4.append(5);
+ll4.append(3);
+ll4.append(2);
+ll4.append(1);
+ll4.append(4);
+ll4.append(3);
+ll4.append(2);
+ll4.append(2);
+ll4.append(6);
+ll4.append(3);
+ll4.append(6);
+console.log("input: ", ll4.showArray(), "solution: ", removeDupBF(ll4).showArray()); // [0, 5, 3, 2, 1, 4, 6]
